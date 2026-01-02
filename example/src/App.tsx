d040e853e69a6b567ddb9b62ebb6a87d6235b9d5
Divyanshu Patil <work.divyanshupatil@gmail.com>
2026-01-02 15:30:24 +0530

    feat: disableImagePasting ios

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -180,7 +180,7 @@ export default function App() {
             fontWeight={'200'}
             fontSize={24}
             color="indigo"
-            // disableImagePasting
+            disableImagePasting={false}
           />
         </View>
         <TextInput
